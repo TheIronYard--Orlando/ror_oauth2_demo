3c4951146ee7ed9c2648bf190754c78fafec601d
Brian Gates <core.safecrlf>
2015-06-25 04:08:10 -0400

    don't store those things here

--- config/initializers/slack_params.rb
+++ config/initializers/slack_params.rb
@@ -1,6 +1,6 @@
 SLACK = {
-  client_id: '3520991751.6829337986',
-  client_secret: '1c797ad6b9bf0f3aca2f1f498bfea7a5',
+  client_id: #don't include this in repo!,
+  client_secret: #or this!,
   team: 'T03FAV5N3',
   scope: 'identify,read,post',
   authorize_uri: 'https://slack.com/oauth/authorize',
